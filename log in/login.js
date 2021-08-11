209a6955ac6c79b52915b7f4838c151adae5f7af
Matthew <mattymallison@gmail.com>
2021-08-11 16:31:12 +0200

    new code

---
+++ log in/login.js
@@ -0,0 +1,75 @@
+const form = [
+  {
+    formImg: "yoco.png",
+    heading: "Yoco point of sale!",
+    subHeading: "Welcome back, Please log in.",
+    action: "#",
+    method: "#",
+    url: "#",
+    btn: "Click me!",
+    backRegister: "Don't have an account?",
+    backUrl: "#",
+  },
+];
+
+function createForm(oneForm) {
+  let createForm = `<img class="login-img" src="${oneForm.formImg}" alt="Yoco logo" />
+    <h1 style="text-transform: uppercase;">${oneForm.heading}</h1>
+    <h3>${oneForm.subHeading}</h3>
+    <form class="login-form" action="${oneForm.action}" method="${oneForm.method}">
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="Username"
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Password"
+        required
+      />
+      <button onclick="logInUser()" class="login-btn" href="${oneForm.url}" type="submit">${oneForm.btn}</button>
+    </form>
+    <a class="register-btn" href="${oneForm.backUrl}">${oneForm.backRegister}</a>`;
+  return createForm;
+}
+
+function displayForm() {
+  let formContainer = document.querySelector(".spikes");
+  for (f of form) {
+    let see = createForm(f);
+    formContainer.innerHTML += see;
+  }
+}
+
+displayForm();
+
+try {
+  function logInUser() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    fetch("https://point-of-sale3.herokuapp.com/auth", {
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        myStorage = window.localStorage;
+        console.log(json["access_token"]);
+        myStorage.setItem("jwt_token", json["access_token"]);
+      });
+    // console.log(username, password);
+  }
+} catch (err) {
+  alert("Invalid, Please enter the valid info or create an");
+}
--- point of sale/index.js
+++ point of sale/index.js
@@ -1,5 +1,5 @@
 let getProductsUrl =
-  "https://point-of-sale-flask-app2.herokuapp.com/show-products/";
+  "https://point-of-sale3.herokuapp.com/show-products/";
 
 function showProducts(url) {
   fetch(url)
@@ -10,9 +10,28 @@ function showProducts(url) {
       let container = document.querySelector(".product-container");
       product.forEach((p) => {
         console.log(p);
-        container.innerHTML += `<h3>${p[2]}</h3><p>${p[3]}</p><h4>${p[4]}</h4>`;
+        container.innerHTML += `<div class="productcard"><img class="productimg" src="" alt="Imgae of the product"><h3 class="productname">${p[2]}</h3><p class="productdes">${p[3]}</p><h4 class="productprice">${p[4]}</h4><button class="select"></button></div>`;
       });
     });
 }
 
 showProducts(getProductsUrl);
+
+function addProduct() {
+  let productName = document.getElementById("product-name").value
+  let productDecription = document.getElementById("product-description").value
+  let productPrice = document.getElementById("product-price").value
+  fetch("https://point-of-sale3.herokuapp.com/add-product/", {
+    method: "POST",
+    body: JSON.stringify({
+      product_name: productName,
+      product_description: productDecription,
+      product_price: productPrice
+    }),
+    headers: {
+      "Content-type": "application/json"
+    }
+  })
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+}
